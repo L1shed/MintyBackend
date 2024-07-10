deb341a0aa25fc770224137eeca8e96eb00ff190
Lished <116368352+L1shed@users.noreply.github.com>
2024-07-10 15:06:06 +0000

    Fresh

--- src/main/kotlin/example/com/Application.kt
+++ src/main/kotlin/example/com/Application.kt
@@ -5,34 +5,6 @@ import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
-
-    embeddedServer(Netty, port = 8080) {
-        install(WebSockets)
-        install(CallLogging)
-
-        routing {
-            webSocket("/chat") {
-                val connections = mutableListOf<WebSocketSession>()
-
-                connections.add(this)
-
-                try {
-                    for (frame in incoming) {
-                        if (frame is Frame.Text) {
-                            val receivedText = frame.readText()
-                            for (connection in connections) {
-                                if (connection != this) {
-                                    connection.send(Frame.Text(receivedText))
-                                }
-                            }
-                        }
-                    }
-                } finally {
-                    connections.remove(this)
-                }
-            }
-        }
-    }.start(wait = true)
 }
 
 fun Application.module() {
